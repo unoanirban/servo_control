494688a1a9813bfe70de984d70a1394a59eddf7d
Anirban Midya <anirbanmidya12@gmail.com>
2024-09-12 21:16:22 +0530

    Update esp_servo.ino

--- esp_servo.ino
+++ esp_servo.ino
@@ -1,20 +1,20 @@
-#include <ESP32Servo.h>
+#include <ESP32Servo.h>              // Install ESP32Servo library from library manager in Arduino IDE or download from github
 
-Servo myServo;
-int servoPin = 18;
+Servo myServo;                       // Create an servo object
+int servoPin = 18;                   // Declare GPIO pin 18 as servoPin
 void setup() 
 {
-  myServo.attach(servoPin);
-  Serial.begin(115200);
+  myServo.attach(servoPin);          // This function links the servo object "myServo" to the pin defined by servoPin 
+  Serial.begin(115200);              // Set the baud rate for serial communication
 }
 
 void loop() 
 {
-for(int a=0; a<=180; a+=10)
+for(int a=0; a<=180; a+=10)          // This is a for loop that iterates over values of a from 0 to 180 in steps of 10 degrees
 {
-  myServo.write(a);
-  Serial.print(a);
-  Serial.print("\n");
-  delay(1000);
+  myServo.write(a);                  // The write() function sends the necessary PWM signal to the servo pin to position the servo to the desired angle
+  Serial.print(a);                   // This prints the current angle a to the Serial Monitor, so you can see what angle the servo is being moved to
+  Serial.print("\n");                
+  delay(1000);                       // This gives the servo time to move to the new position before the next movement occurs
 }
 }
